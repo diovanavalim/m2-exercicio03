549157c8000cc604fa9a6fadb2f8b0b2f9c0dcea
diovanavalim <diovana.valim@mercadolivre.com>
2022-06-27 14:47:44 -0300

    add product array

--- src/Exercicio02/Distribuidora.java
+++ src/Exercicio02/Distribuidora.java
@@ -8,11 +8,13 @@ public class Distribuidora {
         NaoPereciveis amaciante = new NaoPereciveis("lavanderia", "amaciante", 12.23);
         Pereciveis iogurte = new Pereciveis(30, "iogurte", 7.80);
 
-        double precoQueijo = queijo.calcular(5);
-        double precoPresunto = presunto.calcular(5);
-        double precoSabaoEmPo = sabaoEmPo.calcular(5);
-        double precoAmaciante = amaciante.calcular(5);
-        double precoIogurte = iogurte.calcular(5);
+        Produto produtos[] = {queijo, presunto, sabaoEmPo, amaciante, iogurte};
+
+        for (int i = 0; i < produtos.length; i++) {
+            double resultado = produtos[i].calcular(5);
+            System.out.println("Produto: " + produtos[i].getNome() + ", preço final: " + resultado);
+            System.out.println(" ");
+        }
 
         System.out.println("Queijo:");
         System.out.println("Nome do produto: " + queijo.getNome());
@@ -27,13 +29,5 @@ public class Distribuidora {
         System.out.println("Preço do produto: " + amaciante.getPreco());
         System.out.println("Tipo do produto: " + amaciante.getTipo());
         System.out.println("Classe: " + amaciante.getClass());
-
-        System.out.println(" ");
-
-        System.out.println("Preço queijo x5: " + precoQueijo);
-        System.out.println("Preço presunto x5: " + precoPresunto);
-        System.out.println("Preço sabão em pó x5: " + precoSabaoEmPo);
-        System.out.println("Preço amaciante x5: " + precoAmaciante);
-        System.out.println("Preço iogurte x5: " + precoIogurte);
     }
 }
